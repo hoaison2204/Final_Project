f81e123edc4ace1a49523aee97d14cdbda19d524
Hoai Son <hoaison2204@gmail.com>
2022-03-20 18:18:34 +0700

    #video 19

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -3,7 +3,7 @@ let configViewEngine = (app) => {
 
     app.use(express.static("./src/public")) //cấu hình để cho server chỉ được lấy ảnh trong thư mục public
     app.set("view engine", "ejs");
-    app.set("view", "src/views");
+    app.set("views", "src/views");
 }
 
 //export function để các file js khác có thể sử dụng được function này
--- src/route/web.js
+++ src/route/web.js
@@ -1,12 +1,10 @@
 import express from "express";
-
+import homeController from "../controllers/homeController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send("Hello World!")
-    });
-    
+    router.get('/', homeController.getHomePage);
+    router.get('/about', homeController.getAboutPage);
     return app.use("/", router);
 }
 
