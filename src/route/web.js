d43c5497b2dbd9a4aca1b28fd1639f64d18e8aba
Hoai Son <hoaison2204@gmail.com>
2022-04-24 21:07:49 +0700

    #67

--- src/route/web.js
+++ src/route/web.js
@@ -24,6 +24,8 @@ let initWebRoutes = (app) => {
   router.get('/api/get-allcode', userController.getAllCode);
 
   router.get('/api/get-top-doctor-home', doctorController.getTopDoctorHome);
+  router.get('/api/get-all-doctors', doctorController.getAllDoctors);
+  router.post('/api/save-info-doctors', doctorController.postInfoDoctor);
   return app.use("/", router);
 };
 
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,6 +1,5 @@
 import db from "../models/index";
 
-
 let getTopDoctorHome = (limitInput) => {
     return new Promise(async (resolve, reject) => {
         try {
@@ -28,6 +27,53 @@ let getTopDoctorHome = (limitInput) => {
     })
 }
 
+let getAllDoctors = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let doctors = await db.User.findAll({
+                where: { roleId: 'R2' },
+                attributes: {
+                    exclude: ["password", 'image']
+                },
+            })
+            resolve({
+                errCode: 0,
+                data: doctors
+            })
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let saveDetailInfoDoctor = (inputData) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            }
+            else {
+                await db.Markdown.create({
+                    doctorId: inputData.doctorId,
+                    contentHTML: inputData.contentHTML,
+                    contentMarkdown: inputData.contentMarkdown,
+                    description: inputData.description,
+                })
+                resolve({
+                    errCode: 0,
+                    errMessage: 'save successfully!'
+                })
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
 module.exports = {
-    getTopDoctorHome: getTopDoctorHome
+    getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
+    saveDetailInfoDoctor: saveDetailInfoDoctor
 }
